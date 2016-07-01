1b8241efe620c715d235e460b6b55b4170ffa88b
korekontakt <korekontakt@gmail.com>
2016-07-01 18:39:17 +0800

    Use better isnan
    Reference  https://github.com/jbeich/freebsd-ports/blob/torch/math/TH/files/patch-generic_THTensorMath.c

--- lib/TH/generic/THTensorMath.c
+++ lib/TH/generic/THTensorMath.c
@@ -4,6 +4,11 @@
 
 #define TH_OMP_OVERHEAD_THRESHOLD 100000
 
+#if !defined(TH_REAL_IS_FLOAT) && !defined(TH_REAL_IS_DOUBLE)
+#undef isnan
+#define isnan(x) 0
+#endif
+
 void THTensor_(fill)(THTensor *r_, real value)
 {
   TH_TENSOR_APPLY(real, r_,
